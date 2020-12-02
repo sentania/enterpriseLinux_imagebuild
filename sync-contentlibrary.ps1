fdcfd34819c7e015c7097f44f3fdbb61c1037ec3
Scott R. Bowe <scott@sentania.net>
2020-12-02 14:50:08 -0600

    Convert to vSphere Builder

---
+++ sync-contentlibrary.ps1
@@ -0,0 +1,18 @@
+﻿Param(
+  [string]$vSphereUSERNAME,
+  [string]$vSpherePASSWORD
+)
+
+$vCenterlist = get-content inputs/vcenterlist.txt
+foreach ($vcenter in $vCenterlist)
+{
+$cisServerConnection = Connect-CisServer -Server $vcenter -User $vSphereUSERNAME -Password $vSpherePASSWORD
+
+$ContentLibraryService = Get-CisService com.vmware.content.library
+$contentlibrarysubscribedService = Get-CisService com.vmware.content.subscribed_library
+$libraryID = $ContentLibraryService.list().value #this assumes only a single content library
+$contentlibrarysubscribedService.sync($libraryID)
+$contentlibrarysubscribedService.sync($libraryID)
+
+Disconnect-CisServer -Server $cisServerConnection -Confirm:$false
+}
