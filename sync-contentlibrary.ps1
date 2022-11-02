0ad3f2d3c562437cd77b18ee8302949931d226a8
Scott R. Bowe <scott@sentania.net>
2022-11-02 07:58:36 -0500

    .

--- sync-contentlibrary.ps1
+++ sync-contentlibrary.ps1
@@ -2,19 +2,22 @@
   [string]$vSphereUSERNAME,
   [string]$vSpherePASSWORD
 )
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore
+Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+Find-Module "VMware.PowerCLI" | Install-Module -Scope "CurrentUser" -AllowClobber
+Import-Module "VMware.PowerCLI"
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -confirm:$false
 $vCenterlist = get-content inputs/vcenterlist.txt
 
 foreach ($vcenter in $vCenterlist)
 {
 $cisServerConnection = Connect-CisServer -Server $vcenter -User $vSphereUSERNAME -Password $vSpherePASSWORD
 
-$ContentLibraryService = Get-CisService com.vmware.content.library
 $contentlibrarysubscribedService = Get-CisService com.vmware.content.subscribed_library
-$libraryID = $ContentLibraryService.list().value #this assumes only a single content library
-$contentlibrarysubscribedService.sync($libraryID)
+foreach ($library in $contentlibrarysubscribedService.list() )
+{
+  $libraryID =  $library.value
+  $contentlibrarysubscribedService.sync($libraryID)
+}
 
 Disconnect-CisServer -Server $cisServerConnection -Confirm:$false
 }
-
-
